34917512b44d837033bec5694c01809af876f2e0
pylapp <dev@pylapersonne.info>
2018-07-22 18:37:28 +0200

    chore: Android light/full - code clean with spelling issues

--- clients/android/tapsterbot-android/app/src/main/java/pylapp/tapster/client/android/ui/mainscreen/AbstractCommandsFragment.java
+++ clients/android/tapsterbot-android/app/src/main/java/pylapp/tapster/client/android/ui/mainscreen/AbstractCommandsFragment.java
@@ -25,21 +25,8 @@ import android.Manifest;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.ramotion.foldingcell.FoldingCell;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import pylapp.tapster.client.android.R;
 import pylapp.tapster.client.android.networks.HttpClientStub;
@@ -47,7 +34,6 @@ import pylapp.tapster.client.android.tools.Config;
 import pylapp.tapster.client.android.tools.FeaturesFactory;
 import pylapp.tapster.client.android.tools.properties.PropertiesReaderStub;
 import pylapp.tapster.client.android.ui.permissions.PermissionsManagerStub;
-import pylapp.tapster.client.android.ui.taptargets.TapTargetViewBuilder;
 
 /**
  * Abstract fragment for commands to send to the robot
--- clients/android/tapsterbot-android/app/src/main/java/pylapp/tapster/client/android/ui/mainscreen/DrawCommandsFragment.java
+++ clients/android/tapsterbot-android/app/src/main/java/pylapp/tapster/client/android/ui/mainscreen/DrawCommandsFragment.java
@@ -22,29 +22,16 @@
 
 package pylapp.tapster.client.android.ui.mainscreen;
 
-import android.Manifest;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.ramotion.foldingcell.FoldingCell;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import pylapp.tapster.client.android.R;
-import pylapp.tapster.client.android.networks.HttpClientStub;
-import pylapp.tapster.client.android.tools.Config;
-import pylapp.tapster.client.android.ui.taptargets.TapTargetViewBuilder;
 
 /**
  * Fragment containing commands related to robot draws
