77ff8bf8f824e8e5e4edaa32a520ea07a5e1b09a
Pierre-Yves Lapersonne <dev@pylapersonne.info>
2019-08-12 18:14:06 +0200

    chore: android - 🧹  prepare binaries and clean upp
    Signed-off-by: Pierre-Yves Lapersonne <dev@pylapersonne.info>

--- clients/android/tapsterbot-android/app/src/lightDebug/java/pylapp/tapster/client/android/Tapster2ClientApp.kt
+++ clients/android/tapsterbot-android/app/src/lightDebug/java/pylapp/tapster/client/android/Tapster2ClientApp.kt
@@ -49,7 +49,7 @@ import pylapp.tapster.client.android.tools.properties.PropertiesReaderStub
 )
 
 @AcraMailSender(
-        mailTo = "" // TODO Fill this value
+        mailTo = "pylapp.pylapp+tapsterbot@gmail.com" // TODO Fill this value
 )
 
 @AcraNotification(
